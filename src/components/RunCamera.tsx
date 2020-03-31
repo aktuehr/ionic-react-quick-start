92830e387a10347d1d0eeddef6f5058d26baf693
Akito UEHARA <akt.uehr@gmail.com>
2020-03-31 09:33:33 +0900

    feat: カメラを起動してコンポーネント内で扱えるようにしてみた

--- src/components/RunCamera.tsx
+++ src/components/RunCamera.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useCamera, availableFeatures } from '@ionic/react-hooks/camera';
+import { CameraResultType } from "@capacitor/core";
+import { IonButton } from '@ionic/react';
 
 interface RunCameraProps { }
 
 const RunCamera: React.FC<RunCameraProps> = () => {
+    const { photo, getPhoto } = useCamera();
+
+    const handleTakePhoto = () => {
+        if(availableFeatures.getPhoto) {
+            getPhoto({
+                quality: 100,
+                allowEditing: false,
+                resultType: CameraResultType.DataUrl
+            });
+        };
+    };
+
+    console.log(photo);
+
     return (
-        <button>
-            Run Camera
-        </button>
-    )
+        availableFeatures.getPhoto ? (
+            <div>
+                <div><IonButton onClick={handleTakePhoto}>Take Photo</IonButton></div>
+                <div>{photo && <img alt="" src={photo.dataUrl} />}</div>
+            </div>
+        ) :
+            <div>Camera not available on this platform</div>
+    );
 }
 
 export default RunCamera;
