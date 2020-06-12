eb87072aaeef998740a1662e9ab78326475a67ab
Akito UEHARA <akt.uehr@gmail.com>
2020-06-12 22:56:28 +0900

    TabBarのサンプル実装

---
+++ src/pages/PageA/PageA.tsx
@@ -0,0 +1,31 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../../components/ExploreContainer";
+
+const PageA: React.FC = () => {
+  return (
+    <IonPage id="page-1">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>PageA Title1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">PageA Title2</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default PageA;
